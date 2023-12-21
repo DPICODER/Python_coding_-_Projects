818b8da58eface097c3af16027baf627fd8c0c22
DPICODER <varunsaipannala@gmail.com>
2023-12-21 23:40:31 +0530

    basics of print and input

---
+++ Day1_of_100/Basic_ip_op.py
@@ -0,0 +1,13 @@
+# Basic Printing and getting User input
+
+print("Wassup ! new to Python")
+
+
+# Storing the user inputted data into the variable for later use in the scope 
+val = input("Enter your name : ")
+
+val2 = input("now enter your Dogs name for security verification : ")
+
+#printing the both variables using f printing method to include multiple variables at once
+print(f"So your name is ",val," and You have a Dog named ",val2)
+
