39a2d43de77f2bf7975dcf7f9d84b5e5d53acfc7
DPICODER <varunsaipannala@gmail.com>
2024-01-09 21:27:20 +0530

    Reduced code by Using Function call for printing

--- Day4_of_100/game_Rock_Paper_Scissors.py
+++ Day4_of_100/game_Rock_Paper_Scissors.py
@@ -40,26 +40,26 @@ system_inp = int(random.randint(0,2))
 
 user_inp = int(input("Enter your choice \n 0.Rock \n 1.Paper \n 2.Scissor \n:"))
 
+def printing(inp1,inp2):
+    print("Your Choice : " + game_dat[inp1])
+    print("System Played : " + game_dat[inp2])
+
+
 if user_inp <= 2 and system_inp <=2 :
     if user_inp == 0 and system_inp == 1:
-        print("Your Choice : " + game_dat[user_inp])
-        print("System Played : " + game_dat[system_inp])
+        printing(user_inp,system_inp)#Function Call
         print("You lose")
     elif user_inp == 1 and system_inp == 2:
-        print("Your Choice : " + game_dat[user_inp])
-        print("System Played : " + game_dat[system_inp])
+        printing(user_inp,system_inp)
         print("You lose")
     elif user_inp == 2 and system_inp == 0:
-        print("Your Choice : " + game_dat[user_inp])
-        print("System Played : " + game_dat[system_inp])
+        printing(user_inp,system_inp)
         print("You lose")
     elif user_inp == system_inp:
-        print("Your Choice : " + game_dat[user_inp])
-        print("System Played : " + game_dat[system_inp])
+        printing(user_inp,system_inp)
         print("Draw Try Again ")
     else:
-        print("Your Choice : " + game_dat[user_inp])
-        print("System Played : " + game_dat[system_inp])
+        printing(user_inp,system_inp)  
         print("You Win !!")
 else:
     print("Enter input With in Range 0  , 1 , 2")
